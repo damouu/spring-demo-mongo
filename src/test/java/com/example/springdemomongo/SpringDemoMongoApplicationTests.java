1baeed59fc7458323af47df864e09176edf856d2
damouu <mouadsehbaoui@gmail.com>
2022-10-17 19:02:13 +0900

    first commit

---
+++ src/test/java/com/example/springdemomongo/SpringDemoMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
