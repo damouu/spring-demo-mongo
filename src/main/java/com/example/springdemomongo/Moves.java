147af880d4c44f5cd9ecb30836438e416cd051f8
damouu <mouadsehbaoui@gmail.com>
2022-10-19 22:48:14 +0900

    creates the Moves Class Entity

--- src/main/java/com/example/springdemomongo/Moves.java
+++ src/main/java/com/example/springdemomongo/Moves.java
@@ -1,2 +1,15 @@
-package com.example.springdemomongo;public class Moves {
+package com.example.springdemomongo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+
+public class Moves {
+    private List<String> egg;
+    private List<String> gen34;
+    private List<String> level;
+    private List<String> tmhm;
+    private List<String> tutor;
 }
