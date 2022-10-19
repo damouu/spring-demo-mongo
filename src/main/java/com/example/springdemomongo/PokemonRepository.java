63079e3461e7940c77f853bc69da39371b0373f6
damouu <mouadsehbaoui@gmail.com>
2022-10-19 22:48:51 +0900

    creates the PokemonRepository to utilize the Mongo JPA repository.

--- src/main/java/com/example/springdemomongo/PokemonRepository.java
+++ src/main/java/com/example/springdemomongo/PokemonRepository.java
@@ -1,2 +1,6 @@
-package com.example.springdemomongo;public interface PokemonRepository {
+package com.example.springdemomongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PokemonRepository extends MongoRepository<Pokemons, String> {
 }
