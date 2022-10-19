0f360cf3c5bb1cff6d5d7da4586765be7b85ce2b
damouu <mouadsehbaoui@gmail.com>
2022-10-19 22:49:30 +0900

    adds a commandLIneRunner function at the start of the application to populate the Database with some entries/documents.

--- src/main/java/com/example/springdemomongo/SpringDemoMongoApplication.java
+++ src/main/java/com/example/springdemomongo/SpringDemoMongoApplication.java
@@ -1,7 +1,9 @@
 package com.example.springdemomongo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDemoMongoApplication {
@@ -10,4 +12,11 @@ public class SpringDemoMongoApplication {
         SpringApplication.run(SpringDemoMongoApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(PokemonRepository pokemonRepository) {
+        return args -> {
+            Pokemons pokemon = new Pokemons("TEST_ENTRY", "TEST_ENTRY");
+            pokemonRepository.insert(pokemon);
+        };
+    }
 }
