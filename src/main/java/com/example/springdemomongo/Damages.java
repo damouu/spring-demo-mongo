5347b7cfe991ab58bdeae8422395b347ce25280d
damouu <mouadsehbaoui@gmail.com>
2022-10-21 20:01:32 +0900

    SDM-7 Feature: Damages Class
    creation of the Damage Class for the MongoDB entity/Document.

---
+++ src/main/java/com/example/springdemomongo/Damages.java
@@ -0,0 +1,178 @@
+package com.example.springdemomongo;
+
+public class Damages {
+
+    private String bug;
+    private String dark;
+    private String dragon;
+    private String electric;
+    private String fight;
+    private String fire;
+    private String flying;
+    private String ghost;
+    private String grass;
+    private String ground;
+    private String ice;
+    private String normal;
+    private String poison;
+    private String psychic;
+    private String rock;
+    private String steel;
+    private String water;
+
+    public Damages(String bug, String dark, String dragon, String electric, String fight, String fire, String flying, String ghost, String grass, String ground, String ice, String normal, String poison, String psychic, String rock, String steel, String water) {
+        this.bug = bug;
+        this.dark = dark;
+        this.dragon = dragon;
+        this.electric = electric;
+        this.fight = fight;
+        this.fire = fire;
+        this.flying = flying;
+        this.ghost = ghost;
+        this.grass = grass;
+        this.ground = ground;
+        this.ice = ice;
+        this.normal = normal;
+        this.poison = poison;
+        this.psychic = psychic;
+        this.rock = rock;
+        this.steel = steel;
+        this.water = water;
+    }
+
+    public String getBug() {
+        return bug;
+    }
+
+    public void setBug(String bug) {
+        this.bug = bug;
+    }
+
+    public String getDark() {
+        return dark;
+    }
+
+    public void setDark(String dark) {
+        this.dark = dark;
+    }
+
+    public String getDragon() {
+        return dragon;
+    }
+
+    public void setDragon(String dragon) {
+        this.dragon = dragon;
+    }
+
+    public String getElectric() {
+        return electric;
+    }
+
+    public void setElectric(String electric) {
+        this.electric = electric;
+    }
+
+    public String getFight() {
+        return fight;
+    }
+
+    public void setFight(String fight) {
+        this.fight = fight;
+    }
+
+    public String getFire() {
+        return fire;
+    }
+
+    public void setFire(String fire) {
+        this.fire = fire;
+    }
+
+    public String getFlying() {
+        return flying;
+    }
+
+    public void setFlying(String flying) {
+        this.flying = flying;
+    }
+
+    public String getGhost() {
+        return ghost;
+    }
+
+    public void setGhost(String ghost) {
+        this.ghost = ghost;
+    }
+
+    public String getGrass() {
+        return grass;
+    }
+
+    public void setGrass(String grass) {
+        this.grass = grass;
+    }
+
+    public String getGround() {
+        return ground;
+    }
+
+    public void setGround(String ground) {
+        this.ground = ground;
+    }
+
+    public String getIce() {
+        return ice;
+    }
+
+    public void setIce(String ice) {
+        this.ice = ice;
+    }
+
+    public String getNormal() {
+        return normal;
+    }
+
+    public void setNormal(String normal) {
+        this.normal = normal;
+    }
+
+    public String getPoison() {
+        return poison;
+    }
+
+    public void setPoison(String poison) {
+        this.poison = poison;
+    }
+
+    public String getPsychic() {
+        return psychic;
+    }
+
+    public void setPsychic(String psychic) {
+        this.psychic = psychic;
+    }
+
+    public String getRock() {
+        return rock;
+    }
+
+    public void setRock(String rock) {
+        this.rock = rock;
+    }
+
+    public String getSteel() {
+        return steel;
+    }
+
+    public void setSteel(String steel) {
+        this.steel = steel;
+    }
+
+    public String getWater() {
+        return water;
+    }
+
+    public void setWater(String water) {
+        this.water = water;
+    }
+}
